9ffcb5461154e85eb6cad78c0703216d58bf1101
Carlos Sempe <sempejunior@gmail.com>
2018-08-08 00:18:24 -0300

    First Commit

---
+++ src/main/java/com/carlossempe/springbootandmongodb/SpringbootandmongodbApplication.java
@@ -0,0 +1,12 @@
+package com.carlossempe.springbootandmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootandmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootandmongodbApplication.class, args);
+	}
+}
